a5d6dcfb3c1e4e25323548e22ac27d9d907e2448
Tanya Bowerbank <tbowerbank@digis.net>
2018-03-10 10:03:23 -0700

    hi

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -3,18 +3,21 @@ import Header from '../Header/Header';
 import { connect } from 'react-redux';
 import { bindActionCreators} from 'redux';
 import {GetProducts, AddToBasket} from '../../Redux/Actions/action';
-import './Details.css'
+import './Details.css';
+import ratingLarge from '../Images/ratingLarge.gif';
+
 
 class Details extends Component {
     render(){
         return (
             <div>
                <Header showCart={true}/> 
-                <div>
-                    <h1>{this.props.product.name}</h1>
-                    <div className="item-image" style={{backgroundImage: `url("${this.props.product.img}")`}}/>  
-                    <p>{this.props.product.price}</p>                  
-                    <p>{this.props.product.description}</p>
+                <div className="productItem">
+                    <h1 className="item-name">{this.props.product.name}</h1>
+                    <img className="ratingLarge" src={ratingLarge} alt="stars"/>
+                    <div className="imageBlock" style={{backgroundImage: `url("${this.props.product.img}")`}}/>  
+                    <p className="pricingBlock">{'$'}{this.props.product.price}</p>                  
+                    <p className="productContent">{this.props.product.description}</p>
                     <p>{this.props.product.specs}</p>
                 </div>
             </div>
